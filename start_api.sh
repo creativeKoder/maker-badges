b7c44b3e307d92019213ead51d04df73120950b9
josi <iam@josi.io>
2021-04-01 18:33:14 -0400

    adding configuring of the aws table in dynamo

--- start_api.sh
+++ start_api.sh
@@ -2,4 +2,15 @@
 
 . ./packages/api/.env
 
+
+yes y | aws configure
+
+aws dynamodb create-table --table-name maker-badges \
+  --region local \
+  --attribute-definitions AttributeName=templateId,AttributeType=N AttributeName=id,AttributeType=S \
+  --key-schema AttributeName=id,KeyType=HASH \
+  --provisioned-throughput ReadCapacityUnits=25,WriteCapacityUnits=25 \
+  --global-secondary-indexes 'IndexName=templateId-index,KeySchema=[{AttributeName=templateId,KeyType=HASH}],Projection={ProjectionType=ALL},ProvisionedThroughput={ReadCapacityUnits=25,WriteCapacityUnits=25}'
+
+
 yarn api:start
